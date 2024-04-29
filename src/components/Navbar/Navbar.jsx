6ecb2ccb83ed5a4ad42dd8f87b6c9c387e1bec24
Noe Osorio <osorionoe@gmail.com>
2024-04-29 17:26:28 -0600

    Create navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Noe Osorio</h1>
+      </div>
+      <a href="/#">Home</a>
+      <a href="/#">Projects</a>
+      <a href="/#">About Me</a>
+    </nav>
+  );
+};
+
+export default Navbar;
